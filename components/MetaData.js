a239a50f95df49362b39eea7ffe5552386f983d9
gcolombi <gg_1605@hotmail.fr>
2023-01-21 16:55:18 -0500

    fix typo in MetaData component

--- components/MetaData.js
+++ components/MetaData.js
@@ -14,7 +14,7 @@ export default function MetaData({ ...customMeta }) {
     return (
         <Head>
             <meta charSet="utf-8" />
-        	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        	<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>{meta.title}</title>
             <meta name="description" content={meta.description} />
