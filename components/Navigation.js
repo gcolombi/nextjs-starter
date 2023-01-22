37fc912f1699d7f1b7a73ea6b460f43de308c7d5
gcolombi <gg_1605@hotmail.fr>
2023-01-21 22:49:08 -0500

    removed dead code

--- components/Navigation.js
+++ components/Navigation.js
@@ -13,7 +13,6 @@ export default function Navigation() {
          */
         function setNavHeight() {
             setNavigationHeight(navRef.current?.getBoundingClientRect().height);
-            console.log('once');
             return true
         };
 
