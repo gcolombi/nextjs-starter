e6a93e06c2465dcfeea65c584ce990827bc5dfe0
gcolombi <gg_1605@hotmail.fr>
2023-01-26 23:38:14 -0500

    updated & joined classes in Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import styles from '@/styles/modules/Footer.module.scss';
 import NavItem from './NavItem';
+import classNames from 'classnames';
 
 export default function Footer() {
     return (
-        <footer className={`u-blockColor--white ${styles['c-footer']}`}>
+        <footer className={classNames('u-blockColor--white', styles['c-footer'])}>
             <div className="o-container">
                 <div className={styles['c-footer__row']}>
                     <div className={styles['c-footer__list']}>
@@ -22,7 +23,7 @@ export default function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className={`o-wysiwyg ${styles['c-footer__copyright']}`}>
+                    <div className={classNames('o-wysiwyg' , styles['c-footer__copyright'])}>
                         <p>&copy; {new Date().getFullYear()} Faucibus. All rights reserved.</p>
                     </div>
                 </div>
