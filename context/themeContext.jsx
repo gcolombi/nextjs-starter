8a476c9c8c71f486982e347adb137df1dd262b56
gcolombi <gg_1605@hotmail.fr>
2023-01-29 18:17:53 -0500

    themeContext WIP

--- context/themeContext.jsx
+++ context/themeContext.jsx
@@ -1,11 +1,46 @@
 import { createContext, useContext } from 'react';
 
+const colorSchemes = ['light', 'dark'];
+const defaultThemes = ['light', 'dark'];
+const MEDIA = '(prefers-color-scheme: dark)';
+const isServer = typeof window === 'undefined';
 const ThemeContext = createContext(undefined);
+const defaultContext = {
+    setTheme: () => {},
+    themes: []
+};
 
-export function ThemeContextProvider({ children }) {
+export function ThemeContextProvider(props) {
+    const context = useContext(ThemeContext);
+
+    /* Ignore nested context providers, just passthrough children */
+    if (context)
+        return <>{props.children}</>;
+    return <Theme {...props} />;
+}
+
+const Theme = () => {
+
+    return (
+        <ThemeContext.Provider
+            value={{}}
+        >
+            <ThemeScript
+                {
+                    ...{
+
+                    }
+
+                }
+            />
+        </ThemeContext.Provider>
+    )
+}
+
+const ThemeScript = () => {
 
 }
 
 export default function useThemeContext() {
-    return useContext(ThemeContext);
+    return useContext(ThemeContext) ?? defaultContext;
 }
