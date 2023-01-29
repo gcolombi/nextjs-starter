ab28ceb2eb3bd0b12f41f2a44603ef73af669031
gcolombi <gg_1605@hotmail.fr>
2023-01-29 14:51:07 -0500

    useLocalStorage hook WIP

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,6 +1,16 @@
+import { useState } from 'react';
+
 export default function useLocalStorage(
     key,
     initialValue
 ) {
-
+    const [value, setValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.warn(`Error reading localStorage key "${key}":`, error);
+            return initialValue;
+        }
+    });
 }
