dc9f19793575b389737180804945ceef9129ce27
gcolombi <gg_1605@hotmail.fr>
2023-01-29 20:22:32 -0500

    udapted themeContext

--- context/themeContext.jsx
+++ context/themeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { createContext, memo, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 
 const colorSchemes = ['light', 'dark'];
 const defaultThemes = ['light', 'dark'];
@@ -8,6 +8,10 @@ const defaultContext = {
     setTheme: () => {},
     themes: []
 };
+
+export default function useThemeContext() {
+    return useContext(ThemeContext) ?? defaultContext;
+}
 
 export function ThemeContextProvider(props) {
     const context = useContext(ThemeContext);
@@ -45,24 +49,25 @@ const Theme = ({
 
         const name = resolved;
         const enable = disableTransitionOnChange ? disableTransition() : null;
+        const d = document.documentElement;
 
         if (attribute === 'class') {
-            document.documentElement.classList.remove(...attrs);
+            d.classList.remove(...attrs);
 
             if (name)
-                document.documentElement.classList.add(name);
+                d.classList.add(name);
         } else {
             if (name) {
-                document.documentElement.setAttribute(attribute, name);
+                d.setAttribute(attribute, name);
             } else {
-                document.documentElement.removeAttribute(attribute);
+                d.removeAttribute(attribute);
             }
         }
 
         if (enableColorScheme) {
             const fallback = colorSchemes.includes(defaultTheme) ? defaultTheme : null;
             const colorScheme = colorSchemes.includes(resolved) ? resolved : fallback;
-            document.documentElement.style.colorScheme = colorScheme;
+            d.style.colorScheme = colorScheme;
         }
 
         enable?.();
@@ -137,24 +142,111 @@ const Theme = ({
             value={providerValue}
         >
             <ThemeScript
-                {
-                    ...{
-
-                    }
-
-                }
+                {...{
+                    disableTransitionOnChange,
+                    enableSystem,
+                    enableColorScheme,
+                    storageKey,
+                    themes,
+                    defaultTheme,
+                    attribute,
+                    children,
+                    attrs
+                }}
             />
+            {children}
         </ThemeContext.Provider>
     )
 }
 
-const ThemeScript = () => {
+const ThemeScript = memo(({
+    storageKey,
+    attribute,
+    enableSystem,
+    enableColorScheme,
+    defaultTheme,
+    attrs
+}) => {
+    const defaultSystem = defaultTheme === 'system';
 
-}
+    /* Code-golfing the amount of characters in the script */
+    const optimization = (() => {
+        if (attribute === 'class') {
+            const removeClasses = `c.remove(${attrs.map((t) => `'${t}'`).join(',')})`;
 
-export default function useThemeContext() {
-    return useContext(ThemeContext) ?? defaultContext;
-}
+            return `var d=document.documentElement,c=d.classList;${removeClasses};`;
+        } else {
+            return `var d=document.documentElement,n='${attribute}',s='setAttribute';`;
+        }
+    })();
+
+    const fallbackColorScheme = (() => {
+        if (!enableColorScheme) {
+            return '';
+        }
+
+        const fallback = colorSchemes.includes(defaultTheme) ? defaultTheme : null;
+
+        if (fallback) {
+            return `if(e==='light'||e==='dark'||!e)d.style.colorScheme=e||'${defaultTheme}'`;
+        } else {
+            return `if(e==='light'||e==='dark')d.style.colorScheme=e`;
+        }
+    })();
+
+    const updateDOM = (name, literal = false, setColorScheme = true) => {
+        const resolvedName = name;
+        const val = literal ? name + `|| ''` : `'${resolvedName}'`;
+        let text = '';
+
+        /**
+         * Much faster to set colorScheme alongside HTML attribute/class
+         * as it only incurs 1 style recalculation rather than 2
+         * This can save over 250ms of work for pages with big DOM
+         */
+        if (enableColorScheme && setColorScheme && !literal && colorSchemes.includes(name)) {
+            text += `d.style.colorScheme = '${name}';`;
+        }
+
+        if (attribute === 'class') {
+            if (literal || resolvedName) {
+                text += `c.add(${val})`;
+            } else {
+                text += `null`;
+            }
+        } else {
+            if (resolvedName) {
+                text += `d[s](n,${val})`;
+            }
+        }
+
+        return text;
+    }
+
+    const scriptSrc = (() => {
+        if (enableSystem) {
+            return `!function(){try{${optimization}var e=localStorage.getItem('${storageKey}');if('system'===e||(!e&&${defaultSystem})){var t='${MEDIA}',m=window.matchMedia(t);if(m.media!==t||m.matches){${updateDOM(
+                'dark'
+            )}}else{${updateDOM('light')}}}else if(e){${
+                ''
+            }${updateDOM('e', true)}}${
+                !defaultSystem ? `else{` + updateDOM(defaultTheme, false, false) + '}' : ''
+            }${fallbackColorScheme}}catch(e){}}()`
+        }
+
+        return `!function(){try{${optimization}var e=localStorage.getItem('${storageKey}');if(e){${
+            ''
+        }${updateDOM('e', true)}}else{${updateDOM(
+            defaultTheme,
+            false,
+            false
+        )};}${fallbackColorScheme}}catch(t){}}();`;
+    })();
+
+      return <script dangerouslySetInnerHTML={{ __html: scriptSrc }} />
+
+    /* Never re-render this component */
+},() => true);
 
 /**
  * Helpers
