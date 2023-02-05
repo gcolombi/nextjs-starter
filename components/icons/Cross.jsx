c89d3e1a3e8467fbcc5a6354e5eeecdeea44ba7e
gcolombi <gg_1605@hotmail.fr>
2023-02-05 14:24:33 -0500

    added Cross svg component

---
+++ components/icons/Cross.jsx
@@ -0,0 +1,8 @@
+export default function Cross() {
+    return(
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <path d="M 10 10 L 90 90"></path>
+            <path d="M 90 10 L 10 90"></path>
+        </svg>
+    );
+}
