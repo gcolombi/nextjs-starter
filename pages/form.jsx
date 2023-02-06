e47f7d307785809ea2fb0ecd6e9566372cc42acf
gcolombi <gg_1605@hotmail.fr>
2023-02-05 19:22:06 -0500

    updated form page WIP

--- pages/form.jsx
+++ pages/form.jsx
@@ -1,5 +1,10 @@
 import MetaData from '@/components/MetaData';
 import HeaderBasic from '@/components/HeaderBasic';
+import FormInput from '@/components/form/FormInput';
+import FormSelect from '@/components/form/FormSelect';
+import FormCheckboxList from '@/components/form/FormCheckboxList';
+import FormRadioList from '@/components/form/FormRadioList';
+import FormTextarea from '@/components/form/FormTextarea';
 
 export default function FormElements() {
     return(
@@ -14,11 +19,48 @@ export default function FormElements() {
             />
             <section className="u-spacing--responsive--bottom">
                 <div className="o-container">
-                    <div className="o-wysiwyg">
-                        <h2>Form elements</h2>
-                    </div>
+                    <h2>Form elements</h2>
                     <div>
-
+                        <FormInput
+                            htmlFor="firstname"
+                            label="Firstname"
+                            id="firstname"
+                            name="firstname"
+                            required={true}
+                            className="c-formElement--bordered"
+                        />
+                        <FormInput
+                            htmlFor="resume"
+                            label="Resume"
+                            type="file"
+                            id="resume"
+                            name="resume"
+                            required={true}
+                            className="c-formElement--upload--bordered"
+                        />
+                        <FormSelect
+                            htmlFor="subject"
+                            label="Subject"
+                            id="subject"
+                            name="subject"
+                            required={true}
+                            className="c-formElement--select--bordered"
+                        />
+                        <FormCheckboxList
+                            title="Quos fugiat assumenda dolore optio est, corporis sit similique ?"
+                        />
+                        <FormRadioList
+                            title="Quos fugiat assumenda dolore optio est, corporis sit similique ?"
+                            inputName="radio_field_name"
+                        />
+                        <FormTextarea
+                            htmlFor="message"
+                            label="Message"
+                            id="message"
+                            name="message"
+                            required={true}
+                            className="c-formElement--bordered"
+                        />
                     </div>
                 </div>
             </section>
