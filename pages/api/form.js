bf2055576329273bc8796999e9ed76bd009d7b00
gcolombi <gg_1605@hotmail.fr>
2023-02-06 23:45:04 -0500

    form api route WIP

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' });
+    if (req.method = 'POST') {
+
+        const { firstname, lastname } = req.body;
+
+        const message = {
+            firstname,
+            lastname
+        }
+
+        res.status(201).json({ message: 'Thank you, your message has been sent successfully.', message: message });
+    }
 }
