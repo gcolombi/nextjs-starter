534be8ca1b152c9a325fb737a802433be6802863
gcolombi <gg_1605@hotmail.fr>
2023-02-23 23:10:42 -0500

    moved career schema validation in a separate folder

--- components/form/CareerForm.jsx
+++ components/form/CareerForm.jsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { useController } from 'react-hook-form';
 import { useTheme } from 'next-themes';
 import useIsMounted from '@/hooks/useIsMounted';
-import { object, string, mixed, array, addMethod } from 'yup';
+import { careerSchema } from '@/schemas/career';
 import { yupResolver } from '@hookform/resolvers/yup';
 import useUnsavedChanges from '@/hooks/useUnsavedChanges';
 import classNames from 'classnames';
@@ -19,26 +19,6 @@ export const labels = {
     lastname: 'Lastname',
     email: 'Email',
     message: 'Message'
-}
-
-function getFormSchema() {
-    /* override the email method */
-    addMethod(string, 'email', function validateEmail(message){
-        return this.matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, {
-            message,
-            name: 'email',
-        });
-    });
-
-    return object({
-        firstname: string().required('This field is required'),
-        lastname: string().required('This field is required'),
-        email: string().required('This field is required').email('Invalid email address'),
-        resume: mixed().test('required', 'This field is required', (files) => files)
-        .test('fileType', 'Unauthorized format, only jpeg, jpg, png, doc, docx and pdf are valid', (files) => new RegExp(/[^\s]+(.*?).(jpe?g|png|docx?|pdf)$/i).test(files[0]?.name || files.originalFilename))
-        .test('fileSize', 'Max file size 4MB exceeded', (files) => (files[0]?.size || files.size) <= 4 * 1024 * 1024 ),
-        message: string().required('This field is required'),
-    });
 }
 
 async function sendFormData(data) {
@@ -74,7 +54,7 @@ export default function CareerForm() {
             resume: '',
             message: ''
         },
-        // resolver: yupResolver(getFormSchema())
+        resolver: yupResolver(careerSchema)
     });
     const isMounted = useIsMounted();
     const { resolvedTheme } = useTheme();
---
+++ schemas/career.js
@@ -0,0 +1,23 @@
+import { object, string, mixed, addMethod } from 'yup';
+
+const getFormSchema = () => {
+    /* override the email method */
+    addMethod(string, 'email', function validateEmail(message){
+        return this.matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, {
+            message,
+            name: 'email',
+        });
+    });
+
+    return object({
+        firstname: string().required('This field is required'),
+        lastname: string().required('This field is required'),
+        email: string().required('This field is required').email('Invalid email address'),
+        resume: mixed().test('required', 'This field is required', (files) => files)
+        .test('fileType', 'Unauthorized format, only jpeg, jpg, png, doc, docx and pdf are valid', (files) => new RegExp(/[^\s]+(.*?).(jpe?g|png|docx?|pdf)$/i).test(files[0]?.name || files.originalFilename))
+        .test('fileSize', 'Max file size 4MB exceeded', (files) => (files[0]?.size || files.size) <= 4 * 1024 * 1024 ),
+        message: string().required('This field is required'),
+    });
+}
+
+export const careerSchema = getFormSchema();
