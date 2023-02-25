ffa8fb8335129024c735992f437a96a00d2a7cd4
gcolombi <gg_1605@hotmail.fr>
2023-02-24 19:03:50 -0500

    updated parsForm WIP

--- utils/parseForm.js
+++ utils/parseForm.js
@@ -17,6 +17,23 @@ const fileConsumer = (acc) => {
     return writable;
 };
 
-export const parseForm = async (req, opts) => {
+export const parseForm = async (req) => {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+
+        const form = formidable({
+            ...formidableConfig,
+            /* Consumes this, otherwise formidable tries to save the file to disk */
+            fileWriteStreamHandler: () => fileConsumer(chunks)
+        })
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                return reject(err);
+            }
+
+            return resolve({ fields, files });
+        });
+    });
 
 }
