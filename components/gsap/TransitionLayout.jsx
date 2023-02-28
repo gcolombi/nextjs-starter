383905fa6d67ff5e92ee78416676506e38e8d1b9
gcolombi <gg_1605@hotmail.fr>
2023-02-27 20:58:07 -0500

    updated TransitionLayout

--- components/gsap/TransitionLayout.jsx
+++ components/gsap/TransitionLayout.jsx
@@ -1,11 +1,26 @@
 import { gsap } from 'gsap';
 import { TransitionContext } from '@/context/TransitionContext';
-import { useState, useContext, useRef } from 'react';
+import { useState, useContext, useRef, useEffect } from 'react';
 
 export default function TransitionLayout({ children }) {
     const [displayChildren, setDisplayChildren] = useState(children);
     const { timeline } = useContext(TransitionContext);
     const element = useRef();
+
+    useEffect(() => {
+        if (children !== displayChildren) {
+            if (timeline.duration() === 0) {
+                /* There are no outro animations, so immediately transition */
+                setDisplayChildren(children);
+            } else {
+                timeline.play().then(() => {
+                    /* Outro complete so reset to an empty paused timeline */
+                    timeline.seek(0).pause().clear();
+                    setDisplayChildren(children);
+                });
+            }
+        }
+    }, [children]);
 
     return (
         <div ref={element}>{displayChildren}</div>
