b80a0d3196db2e3362e85f187c3359ad3825285b
gcolombi <gg_1605@hotmail.fr>
2023-02-27 20:50:43 -0500

    added TransitionLayout component

---
+++ components/gsap/TransitionLayout.jsx
@@ -0,0 +1,13 @@
+import { gsap } from 'gsap';
+import { TransitionContext } from '../context/TransitionContext';
+import { useState, useContext, useRef } from 'react';
+
+export default function TransitionLayout({ children }) {
+    const [displayChildren, setDisplayChildren] = useState(children);
+    const { timeline, background } = useContext(TransitionContext);
+    const element = useRef();
+
+    return (
+        <div ref={element}>{displayChildren}</div>
+    );
+}
