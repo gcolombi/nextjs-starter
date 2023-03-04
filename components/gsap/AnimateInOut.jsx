e298dc63b8280b2b25b50888c009d8dc3ce86ce8
gcolombi <gg_1605@hotmail.fr>
2023-03-03 21:29:32 -0500

    added AnimateInOut component

---
+++ components/gsap/AnimateInOut.jsx
@@ -0,0 +1,52 @@
+import { gsap } from 'gsap';
+import React, { useRef } from 'react';
+import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
+import useTransitionContext from '@/context/transitionContext';
+
+export default function AnimationInOut({
+    children,
+    from,
+    to,
+    durationIn,
+    durationOut,
+    delay = 0,
+    delayOut = 0,
+    set,
+    skipOutro,
+}) {
+    const { timeline } = useTransitionContext();
+    const element = useRef();
+
+    useIsomorphicLayoutEffect(() => {
+        /* Intro animation */
+        if (set) {
+            gsap.set(element.current, { ...set });
+        }
+
+        gsap.to(element.current, {
+            ...to,
+            delay: delay,
+            duration: durationIn
+        });
+
+        /* Outro animation */
+        if (!skipOutro) {
+            timeline.add(
+                gsap.to(element.current, {
+                    ...from,
+                    delay: delayOut,
+                    duration: durationOut
+                }),
+                0
+            )
+        }
+    }, [])
+
+    return (
+        <div ref={element}>
+            {children}
+        </div>
+    )
+}
+
+// export default React.memo(AnimateInOut)
