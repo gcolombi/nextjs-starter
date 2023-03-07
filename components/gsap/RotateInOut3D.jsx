37924dff3f74a314cb2c02dbbffb547f08a5f49d
gcolombi <gg_1605@hotmail.fr>
2023-03-07 18:46:12 -0500

    removed dead code

--- components/gsap/RotateInOut3D.jsx
+++ components/gsap/RotateInOut3D.jsx
@@ -1,5 +1,4 @@
 import { gsap } from 'gsap';
-import AnimateInOut from './AnimateInOut';
 import { useRef } from 'react';
 import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
 import useTransitionContext from '@/context/transitionContext';
@@ -35,14 +34,6 @@ export default function RotateInOut3D({
                     perspective: 10,
                     scale: 0.8,
                     opacity: 0
-                    // x: randomNumber(-2000, 2000),
-                    // y: randomNumber(-1000, 1000),
-                    // z: randomNumber(100, 100),
-                    // opacity: 0,
-                    // rotation: randomNumber(360, 720),
-                    // rotationX: randomNumber(-360, 360),
-                    // rotationY: randomNumber(-360, 360),
-                    // ease
                 },
                 {
                     x: 0,
@@ -55,16 +46,6 @@ export default function RotateInOut3D({
                     ease,
                     delay,
                     duration: durationIn
-                    // x: 0,
-                    // y: 0,
-                    // z: 0,
-                    // rotation: 0,
-                    // rotationX: 0,
-                    // rotationY: 0,
-                    // opacity: 1,
-                    // ease,
-                    // delay,
-                    // duration: durationIn
                 }
             )
 
@@ -83,16 +64,6 @@ export default function RotateInOut3D({
                             ease: easeOut,
                             delay: delayOut,
                             duration: durationOut
-                            // x: randomNumber(-2000, 2000),
-                            // y: randomNumber(-1000, 1000),
-                            // z: randomNumber(100, 100),
-                            // opacity: 0,
-                            // rotation: randomNumber(360, 720),
-                            // rotationX: randomNumber(-360, 360),
-                            // rotationY: randomNumber(-360, 360),
-                            // ease: easeOut,
-                            // delay: delayOut,
-                            // duration: durationOut
                         }
                     ),
                     0
@@ -103,51 +74,8 @@ export default function RotateInOut3D({
     }, [])
 
     return (
-
         <div ref={element} style={{ opacity: 0 }}>
             {children}
         </div>
-
-        // <AnimateInOut
-        //     durationIn={durationIn}
-        //     durationOut={durationOut}
-        //     delay={delay}
-        //     delayOut={delayOut}
-        //     set={{
-        //         opacity: 0,
-        //         scale: 0.8,
-        //         transformOrigin: '50% 50%',
-        //         perspective: 10,
-        //         rotationX: randomNumber(-80, 80),
-        //         rotationY: randomNumber(-40, 40),
-        //         rotationZ: randomNumber(-10, 10),
-        //         x,
-        //         y
-        //     }}
-        //     from={{
-        //         opacity: 0,
-        //         rotationX: randomNumber(-80, 80),
-        //         rotationY: randomNumber(-40, 40),
-        //         rotationZ: randomNumber(-10, 10),
-        //         x,
-        //         y
-        //         // transform: `translate(${x}px, ${y}px) rotate3d(${randomNumber(-80, 80)}, ${randomNumber(-40, 40)}, ${randomNumber(-10, 10)}, 0)`
-        //     }}
-        //     to={{
-        //         ease,
-        //         opacity: 1,
-        //         scale: 1,
-        //         rotationX: 0,
-        //         rotationY: 0,
-        //         rotationZ: 0,
-        //         x: xTo,
-        //         y: yTo
-        //     }}
-
-
-        //     skipOutro={skipOutro}
-        // >
-        //     {children}
-        // </AnimateInOut>
     );
 };
