f3c5d95a78cdf5da1bf5ee52583e9ea1b8cd0497
gcolombi <gg_1605@hotmail.fr>
2023-03-09 23:42:26 -0500

    removed dead code

--- components/NavItem.jsx
+++ components/NavItem.jsx
@@ -4,7 +4,6 @@ import { useRef } from 'react';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
 import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
-import useTransitionContext from '@/context/transitionContext';
 
 export default function NavItem({
     href,
@@ -13,19 +12,15 @@ export default function NavItem({
     className,
     overflowHidden,
     durationIn = 0.35,
-    durationOut = 0.25,
     delay = 0.5,
-    delayOut = 0,
     ease = 'sine.out',
     rotate = 0,
     scale = 1,
     x = 0,
     y = 0,
-    skipOutro
 }) {
     const router = useRouter();
     const isActive = router.asPath === href;
-    const { timeline } = useTransitionContext();
     const element = useRef()
 
     const from = {
@@ -46,18 +41,6 @@ export default function NavItem({
                 delay,
                 duration: durationIn
             });
-
-            /* Outro animation */
-            // if (!skipOutro) {
-            //     timeline.add(
-            //         gsap.to(element.current,{
-            //             ...from,
-            //             delay: delayOut,
-            //             duration: durationOut
-            //         }),
-            //         0
-            //     );
-            // }
         }, element);
         return () => ctx.revert();
     }, []);
