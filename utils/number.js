b6e8c70722029d83c2ee6e166635b706a23845be
gcolombi <gg_1605@hotmail.fr>
2023-03-10 20:34:09 -0500

    commented number util

--- utils/number.js
+++ utils/number.js
@@ -1,3 +1,9 @@
+/**
+ * Generates a random number using a minimum and maximum value
+ * @param {Number} min minimum value
+ * @param {Number} max maximum value
+ * @returns {Number} a random number
+ */
 export const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
