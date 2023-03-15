4281a28097a6ae4f85a3b870526d595beb945711
gcolombi <gg_1605@hotmail.fr>
2023-03-15 01:05:16 -0400

    updated Accordion & AccordionItem WIP

--- components/accordion/Accordion.jsx
+++ components/accordion/Accordion.jsx
@@ -1,7 +1,29 @@
-export default function Accordion({
+import AccordionItem from './AccordionItem';
 
+const items = [
+    {
+        header: 'What is Lorem Ipsum?',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing...'
+    },
+    {
+        header: 'Where does it come from?',
+        content: 'Quisque eget luctus mi, vehicula mollis lorem...'
+    },
+    {
+        header: 'Why do we use it?',
+        content: 'Suspendisse massa risus, pretium id interdum in...'
+    }
+];
+export default function Accordion({
+    items
 }) {
     return (
-        <div></div>
+        <div className="c-accordions">
+            {items.map(({ header, content }, i) => (
+                <AccordionItem header={header} id={i}>
+                    {content}
+                </AccordionItem>
+            ))}
+        </div>
     );
 }
--- components/accordion/AccordionItem.jsx
+++ components/accordion/AccordionItem.jsx
@@ -1,7 +1,44 @@
 export default function AccordionItem({
-
+    header,
+    id,
+    children
 }) {
     return (
-        <div></div>
+        <div class="c-accordions__item">
+            <Heading
+                header={header}
+                id={id}
+            />
+            <div class="c-accordions__inner">
+                <div>
+                    <div class="o-wysiwyg">
+                        <p>{children}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Heading({
+    header,
+    headingTag = 'h3',
+    id
+}) {
+    const validHeadingTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    const safeHeading = headingTag ? headingTag.toLowerCase() : '';
+    const Heading = validHeadingTags.includes(safeHeading) || 'h3';
+
+    return(
+        <Heading>
+            <button
+                type="button"
+                id={id}
+                aria-expanded=""
+                onClick={() => console.log('clicked')}
+            >
+                {header}
+            </button>
+        </Heading>
     );
 }
