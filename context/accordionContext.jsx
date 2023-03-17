18fdcb2547d41c6b8e5e965514f0f99294ced709
gcolombi <gg_1605@hotmail.fr>
2023-03-16 23:32:19 -0400

    updated accordionContext - missing import

--- context/accordionContext.jsx
+++ context/accordionContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useCallback, useContext, useState } from 'react';
 
 const AccordionContext = createContext();
 
