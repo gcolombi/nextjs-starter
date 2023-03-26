3e08ed0cca5a2e8fd742d7a243d3e4a1970671f7
gcolombi <gg_1605@hotmail.fr>
2023-03-25 21:41:40 -0400

    updated DemoModal style WIP

--- components/modal/DemoModal.jsx
+++ components/modal/DemoModal.jsx
@@ -1,3 +1,4 @@
+import styles from '@/styles/modules/DemoModal.module.scss';
 import { useState, useCallback, useMemo } from 'react';
 import Modal from './Modal';
 
@@ -24,8 +25,8 @@ function DemoModal({
 }) {
     return (
         <Modal showModal={showDemoModal} setShowModal={setShowDemoModal}>
-            <div className="c-demoModal">
-                <div className="c-demoModal__inner">
+            <div className={styles['c-demoModal']}>
+                <div className={styles['c-demoModal__inner']}>
                     <h2>Demo modal</h2>
                     <div className="o-wysiwyg">
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis quaerat corporis ratione blanditiis omnis neque! Nihil rem, tenetur unde error labore, dolores assumenda cupiditate voluptatem aliquid iste ut, natus perspiciatis!</p>
